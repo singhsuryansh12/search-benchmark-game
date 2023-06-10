d6a4cae8cdfce9f0fb85f42d62fd6aa34280b3fc
Mike McCandless <mikemccand@apache.org>
2023-06-10 14:18:23 -0400

    fix run_many to save to results_many dir

--- src/run_many.py
+++ src/run_many.py
@@ -7,6 +7,8 @@ query_file = sys.argv[1]
 iters = int(sys.argv[2])
 engines = sys.argv[3:]
 
+RESULTS_OUTPUT_DIR = 'results_many'
+
 print(f'Running bench {iters} times, using queries from {query_file}, testing engines {" ".join(engines)}')
 
 for i in range(10):
@@ -21,8 +23,8 @@ for i in range(10):
     except subprocess.CalledProcessError as e:
         sys.stderr.write(e.stderr.decode('utf-8'))
         raise
-    os.rename('results.json', f'results/results{i}.json')
-    open(f'results/stdout{i}.txt', 'wb').write(result.stdout)
-    open(f'results/stderr{i}.txt', 'wb').write(result.stderr)
+    os.rename('results.json', f'{RESULTS_OUTPUT_DIR}/results{i}.json')
+    open(f'{RESULTS_OUTPUT_DIR}/stdout{i}.txt', 'wb').write(result.stdout)
+    open(f'{RESULTS_OUTPUT_DIR}/stderr{i}.txt', 'wb').write(result.stderr)
     t1 = time.time()
     print(f'  done!  [{t1 - t0:.2f}] seconds')
